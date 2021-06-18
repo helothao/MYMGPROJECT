d6466eb3a638a26f024a4a1134678f324bea127f
Cyprien <cyprienthao@gmail.com>
2021-06-18 13:44:55 +0200

    BDD CONFIG

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php 
+    // INFOS CONNEXION A LA BDD MYSQL
+    $host = "sql289.main-hosting.eu";
+    $dbname = "u601074896_mymgproject";
+    $mysql_user = "u601074896_mymg";
+    $mysql_password ="Azerty$1";
+
+?>
---
+++ config/dbconnexion.php
@@ -0,0 +1,10 @@
+<?php
+    require('config.php');
+
+    try {
+        $db = new PDO('mysql:host=' . $host . ';dbname=' . $dbname .';charset=utf8', $mysql_user, $mysql_password);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+?>
