2e23dc89f421299709385a95b40c142ca9b21183
helothao <thaoheloise@gmail.com>
2021-06-18 17:40:56 +0200

    COMMIT

--- comparateur.php
+++ comparateur.php
@@ -9,9 +9,75 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" src="./styles/comparateur.css" />
+    <link rel="stylesheet" href="./styles/comparateur.css" />
 </head>
 <body>
+    <div class="formulaireFiltres">
+        <form action="" method="GET" class="formulaireEmail"> 
+            <div>
+                <label for="type"> Vous recherchez une banque pour : </label>
+                <select name="type">
+                    <option value="">--Selectionnez une option</option>
+                    <option value="Investir">Investir</option>
+                    <option value="Epargner">Epargner</option>
+                </select>
+                
+            </div>
 
+            <div>
+                <label for="theme"> Sélectionnez les valeurs qui vous sont chères : </label>
+                <select name="theme">
+                    <option value="">--Selectionnez une option</option>
+                    <option value="Engagement environnemental">Engagement environnemental</option>
+                    <option value="Engagement social">Engagement social</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="montant"> Saisissez un montant (€) : </label>
+                <input name="montant" type="number" />
+            </div>
+
+
+            <button>RECHERCHER</button>
+        </form>
+    </div>
+
+    <?php if(isset($_GET['type']) || isset($_GET['theme']) || isset($_GET['montant'])) { ?>
+        <div class="conteneurBanques">
+            <?php
+
+                    $type = $_GET['type'];
+                    $montant = $_GET['montant'];
+                    $theme = $_GET['theme'];
+
+                    echo $type;
+
+                    $requete = 'SELECT * FROM banques'
+                            . ($type || $montant || $theme ? ' WHERE ' : '')
+                            . ($type ? "type = '" . $type . "'"  . ($montant || $theme ? ' AND ' : '') : '')
+                            . ($montant ? "placementMin <= " . $montant . ' && plafond >= ' . $montant . "" . ($theme ? ' AND ' : '') : '')
+                            . ($theme ? "theme = '" . $theme . "'" : '')
+                            . ';';
+
+                    $req = $bdd->query($requete); // WHERE email = :email ORDER BY datearrivee
+                    while($donnees = $req->fetch()) { ?>
+                        <div class="blocBanque">
+                            <h3 style="padding:0;margin:0;"><?php echo $donnees['nom'] ?></h3>
+                            <hr/>
+                            <ul>
+                                <li><b>Valeurs : </b> <?php echo $donnees['valeurs'] ?> </li>
+                                <li><b>Type d'actif : </b> <?php echo $donnees['actif'] ?> </li>
+                                <li><b>Plafond : </b> <?php echo $donnees['plafond'] ?> </li>
+                                <li><b>Taux brut pour 1 an (%) : </b> <?php echo $donnees['taux'] ?> </li>
+                                <li><b>Niveau de risque : </b> <?php echo $donnees['risque'] ?> </li>
+                                <li><b>Engagements : </b> <?php echo $donnees['engagements'] ?> </li>
+
+                            </ul>
+                        </div>
+                        
+            <?php } ?>
+        </div>
+    <?php } ?>
 </body>
 </html>
---
+++ composants/navbar.php
@@ -0,0 +1,57 @@
+<header class="u-clearfix u-header u-header" id="sec-238b">
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1326" data-image-height="882">
+        <img src="images/200930789_200446098612759_2343269356248476456_n.png" class="u-logo-image u-logo-image-1"
+          data-image-width="116" />
+      </a>
+      <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+        <div class="menu-collapse u-custom-font u-font-courier-new" style="font-size: 1rem; letter-spacing: 0px">
+          <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
+            href="#">
+            <svg>
+              <use xlink:href="#menu-hamburger"></use>
+            </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <defs>
+                <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px">
+                  <rect y="1" width="16" height="2"></rect>
+                  <rect y="7" width="16" height="2"></rect>
+                  <rect y="13" width="16" height="2"></rect>
+                </symbol>
+              </defs>
+            </svg>
+          </a>
+        </div>
+        <div class="u-custom-menu u-nav-container">
+          <ul class="u-custom-font u-font-courier-new u-nav u-unstyled u-nav-1">
+            <li class="u-nav-item">
+              <a class="u-button-style u-nav-link u-text-active-palette-4-dark-2 u-text-hover-palette-4-dark-1"
+                href="Trouver-une-banque.html" style="padding: 10px 20px">Trouver une banque</a>
+            </li>
+            <li class="u-nav-item">
+              <a class="u-button-style u-nav-link u-text-active-palette-4-dark-2 u-text-hover-palette-4-dark-1"
+                href="A-propos-du-projet.html" style="padding: 10px 20px">A propos du projet</a>
+            </li>
+          </ul>
+        </div>
+        <div class="u-custom-menu u-nav-container-collapse">
+          <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+            <div class="u-sidenav-overflow">
+              <div class="u-menu-close"></div>
+              <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                <li class="u-nav-item">
+                  <a class="u-button-style u-nav-link" href="Trouver-une-banque.html" style="padding: 10px 20px">Trouver
+                    une banque</a>
+                </li>
+                <li class="u-nav-item">
+                  <a class="u-button-style u-nav-link" href="A-propos-du-projet.html" style="padding: 10px 20px">A
+                    propos du projet</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+        </div>
+      </nav>
+    </div>
+  </header>
